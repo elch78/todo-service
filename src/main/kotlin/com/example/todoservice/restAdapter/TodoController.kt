a8563924951ed88afc0fa67127b414c6a3fa741a
Nils Rommelfanger <nils.rommelfanger@beyonnex.io>
2024-04-09 22:37:34 +0200

    test dueAt must not be in the past (green)

--- src/main/kotlin/com/example/todoservice/restAdapter/TodoController.kt
+++ src/main/kotlin/com/example/todoservice/restAdapter/TodoController.kt
@@ -2,10 +2,13 @@ package com.example.todoservice.restAdapter
 
 import com.example.todoservice.TimeProvider
 import com.example.todoservice.core.TodoItem
-import com.example.todoservice.core.TodoService
+import com.example.todoservice.core.TodoRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
+import org.springframework.web.ErrorResponseException
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,18 +17,23 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/todo")
 class TodoController @Autowired constructor(
-    private val service: TodoService,
+    private val repo: TodoRepository,
     private val timeProvider: TimeProvider,
 ) {
     @PostMapping(consumes = ["application/json"], produces = ["application/json"])
     fun new(@RequestBody dto: NewTodoDto): ResponseEntity<TodoItemDto> {
+
         val now = timeProvider.now();
+        if(dto.dueAt.isBefore(now)) {
+            throw ErrorResponseException(BAD_REQUEST, ProblemDetail.forStatusAndDetail(BAD_REQUEST, "dueAt must not be in the past: ${dto.dueAt}" ), null)
+        }
+
         val todoItem = TodoItem(
             description = dto.description,
             createdAt = now,
             dueAt = dto.dueAt
         )
-        val newTodoItem = service.new(todoItem)
+        val newTodoItem = repo.new(todoItem)
         return ResponseEntity.status(CREATED)
             .body(TodoItemDto.from(newTodoItem, now))
     }
