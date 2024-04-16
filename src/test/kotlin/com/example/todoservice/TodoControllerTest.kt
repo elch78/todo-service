9b521ab6b4d6c63bf9ca3c06c1c460eabd476c32
Nils Rommelfanger <nils.rommelfanger@beyonnex.io>
2024-04-16 09:25:33 +0200

    add javadoc to TodoControllerTest

--- src/test/kotlin/com/example/todoservice/TodoControllerTest.kt
+++ src/test/kotlin/com/example/todoservice/TodoControllerTest.kt
@@ -36,6 +36,9 @@ import java.time.temporal.ChronoUnit.MINUTES
 import java.util.*
 import java.util.stream.Stream
 
+/**
+ * Test the todo-api end to end
+ */
 @SpringBootTest
 @AutoConfigureMockMvc
 // Transactional + Rollback
